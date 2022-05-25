194aac82c970deb04ed63c529f63ff28c4cfdbdb
pyaephyomaung <ppmaungdev@gmail.com>
2022-05-25 20:55:51 +0630

    test tailwind css with background gradient and heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className='bg-red-100'>test</div>
+      <div className='bg-gradient-to-br from-[#87cf8e] to-[#b8cf87] min-h-screen'>
+        <h2 className='text-4xl font-medium text-slate-800 p-3'>
+          React Kurrency
+        </h2>
+
+      </div>
     </div>
   );
 }
