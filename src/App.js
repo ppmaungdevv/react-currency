da3ec46a3cda98b8b9b0093e9b229d515fe4a5cc
pyaephyomaung <ppmaungdev@gmail.com>
2022-05-25 20:34:57 +0630

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-red-100'>test</div>
+    </div>
+  );
+}
+
+export default App;
