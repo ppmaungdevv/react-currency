a92b83eeb18c8ac6e1fd6feb6e52e12b2b5d9dc3
pyaephyomaung <ppmaungdev@gmail.com>
2022-05-28 14:31:20 +0630

    base UI setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -8,13 +7,24 @@ function App() {
         <h2 className='p-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-[#b8cf87] to-[#87cf8e]'>
           React Kurrency
         </h2>
-        <select className='appearance-none whitespace-nowrap px-3 py-2.5 bg-white leading-tight text-base font-medium text-center uppercase rounded-l border-l border-y text-green-800 focus:outline-none focus:ring-0 focus:bg-gray-100 ' name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
-        </select>
-
+        <div className='p-3'>
+          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars">
+            <option className='' value="volvo">USD</option>
+            <option value="saab">THB</option>
+            <option value="mercedes">MMK</option>
+            <option className='py-2.5' value="audi">SGD</option>
+          </select>
+          <input className='bg-white p-2 border rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
+        </div>
+        <div className='p-3'>
+          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md transition ease-in-out' name="cars" id="cars">
+            <option value="saab">Myanmar Kyats</option>
+            <option value="volvo">United States Dollar</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+          <input className='bg-white p-2 border rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
+        </div>
       </div>
     </div>
   );
