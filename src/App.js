2af6f988cb1384970d858db54d2950a1c58aeffd
pyaephyomaung <ppmaungdev@gmail.com>
2022-06-03 20:26:34 +0630

    active nav bar style and change font color

--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ function App() {
   const changeComponent = (comp_name) => {
     set_comp(comp_name)
   }
-
   return (
     <div className="App">
       <div className='bg-gradient-to-br from-[#eeeced] to-[#e6f0ff] min-h-screen'>
         {/* <h2 className='text-4xl font-medium text-slate-800 p-3'> */}
-        <h2 className='p-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-[#b8cf87] to-[#87cf8e]'>
+        <h2 className='p-2 font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-400'>
           React Kurrency
         </h2>
-        <NavBar changeComponent={changeComponent} />
+        <NavBar changeComponent={changeComponent} comp={comp} />
         <BaseComponent comp={comp} />
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
 const NavBar = (props) => {
-    const { changeComponent } = props
+    const { changeComponent, comp } = props
+    const nav_class = 'flex-1 p-1 m-1 hover:cursor-pointer hover:border-b hover:border-b-emerald-600',
+    nav_active_class = 'border-b border-b-emerald-600', item_class = 'p-2 text-lg font-medium hover:text-emerald-600'
   return (
     <div className='flex justify-center m-2'>
         <div className='flex w-2/3'>
-        {/* active style */}
-        <div onClick={() => changeComponent('converter')} className='flex-1 m-1 p-1 border-b border-green-500 hover:cursor-pointer'>
-            <h4 className='p-2 text-lg text-green-500'>Converter</h4>
-        </div>
-        <div onClick={() => changeComponent('graph')} className='flex-1 p-1 m-1 hover:cursor-pointer hover:border-b hover:border-b-green-500'>
-            <h4 className='p-2 text-lg text-green-400 hover:text-green-500'>Graph</h4>
-        </div>
-        <div onClick={() => changeComponent('list')} className='flex-1 p-1 hover:cursor-pointer hover:border-b hover:border-b-green-500'>
-            <h4 className='p-2 text-lg text-green-400 hover:text-green-500'>All Exchange Rate</h4>
-        </div>
+            <div onClick={() => changeComponent('converter')} className={`${nav_class}  ${comp === 'converter' ? nav_active_class : ''}`}>
+                <h4 className={`${item_class} ${comp === 'converter' ? 'text-emerald-700' : 'text-emerald-500'}`}>Converter</h4>
+            </div>
+            <div onClick={() => changeComponent('graph')} className={`${nav_class} ${comp === 'graph' ? nav_active_class : ''}`}>
+                <h4 className={`${item_class} ${comp === 'graph' ? 'text-emerald-700' : 'text-emerald-500'}`} >Graph</h4>
+            </div>
+            <div onClick={() => changeComponent('list')} className={`${nav_class} ${comp === 'list' ? nav_active_class : ''}`}>
+                <h4 className={`${item_class} ${comp === 'list' ? 'text-emerald-700' : 'text-emerald-500'}`}>All Exchange Rates</h4>
+            </div>
         </div>
     </div>
   )
