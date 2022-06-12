8d416bcf709acc368e65ea11df641faf5d41abd9
pyaephyomaung <ppmaungdev@gmail.com>
2022-06-12 20:37:48 +0630

    initialize exchange list

--- src/components/ConverterComponent.js
+++ src/components/ConverterComponent.js
@@ -10,7 +10,7 @@ const ConverterComponent = () => {
           <option value="mercedes">MMK</option>
           <option className='py-2.5' value="audi">SGD</option>
         </select>
-        <input className='w-full lg:w-2/3 bg-white p-2 border border-emerald-500 rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
+        <input className='w-full md:w-2/3 bg-white p-2 border border-emerald-500 rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
       </div>
       <div className='flex justify-center'>
         <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars">
@@ -19,7 +19,7 @@ const ConverterComponent = () => {
           <option value="mercedes">MMK</option>
           <option className='py-2.5' value="audi">SGD</option>
         </select>
-        <input className='w-full lg:w-2/3 bg-white p-2 border border-emerald-500 rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
+        <input className='w-full md:w-2/3 bg-white p-2 border border-emerald-500 rounded ml-2 focus:outline-none focus:border-green-300 focus:shadow-md' type='text' />
       </div>
     </div>
   )
--- src/components/ExchangeListComponent.js
+++ src/components/ExchangeListComponent.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const ExchangeListComponent = () => {
+  const [base_currency, setBaseCurrency] = useState({
+    'MMK': 'Myanmar Kyats',
+    'USD': 'United States Dollar'
+  })
+  const [currencies, setCurrencies] = useState({})
+
   return (
-    <div>ExchangeListComponent</div>
+    <div className='w-full'>
+      <div className='flex p-3 w-full place-content-center'>
+        <div className='flex place-content-center w-1/3'>
+          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars">
+              {
+                Object.keys(base_currency).map(ele => <option value={ele} key={ele}>{base_currency[ele]}</option>)
+              }
+              {/* <option value="saab">Thai Bhat</option> */}
+          </select>
+        </div>
+      </div>
+    </div>
   )
 }
 
