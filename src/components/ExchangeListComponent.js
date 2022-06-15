9da60148cf739aa92e59b1f8383c0ebbf671778c
pyaephyomaung <ppmaungdev@gmail.com>
2022-06-15 19:31:40 +0630

    get all currency symbols from api

--- src/components/ExchangeListComponent.js
+++ src/components/ExchangeListComponent.js
@@ -1,10 +1,24 @@
+import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 
+
 const ExchangeListComponent = () => {
-  const [base_currency, setBaseCurrency] = useState({
-    'MMK': 'Myanmar Kyats',
-    'USD': 'United States Dollar'
-  })
+  const [base_currency, setBaseCurrency] = useState([])
+  useEffect(() => {
+    let temp = null
+    axios.get('https://api.exchangerate.host/symbols').then(({data}) => {
+      const test = Object.keys(data.symbols).map(ele => {
+        const { description, code } = data.symbols[ele]
+        return { description, code }
+      })
+      setBaseCurrency(test)
+    })
+  
+    // return () => {
+    //   second
+    // }
+  }, [])
+  
   const [currencies, setCurrencies] = useState({})
 
   return (
@@ -13,7 +27,7 @@ const ExchangeListComponent = () => {
         <div className='flex place-content-center w-1/3'>
           <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars">
               {
-                Object.keys(base_currency).map(ele => <option value={ele} key={ele}>{base_currency[ele]}</option>)
+                base_currency.map(ele => <option value={ele.code} key={ele.code}>{ele.description}</option>)
               }
               {/* <option value="saab">Thai Bhat</option> */}
           </select>
