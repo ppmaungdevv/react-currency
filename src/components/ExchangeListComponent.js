48c848f8cb1ece1e1127e1ffd92b97f0fa56c8f1
pyaephyomaung <ppmaungdev@gmail.com>
2022-06-15 20:11:47 +0630

    fetch data when selected value changed

--- src/components/ExchangeListComponent.js
+++ src/components/ExchangeListComponent.js
@@ -18,14 +18,32 @@ const ExchangeListComponent = () => {
     //   second
     // }
   }, [])
+
+  const [selected_base, setSelectedBase] = useState('MMK')
+  useEffect(() => {
+    
+    axios.get(`https://api.exchangerate.host/latest?base=${selected_base}`).then(({data}) => {
+      console.log('refetch', data)
+    })
+
+    // return () => {
+    //   second
+    // }
+  }, [selected_base])
   
+  
+
+  const changeBase = e => {
+    console.log(e.target.value)
+  }
+
   const [currencies, setCurrencies] = useState({})
 
   return (
     <div className='w-full'>
       <div className='flex p-3 w-full place-content-center'>
         <div className='flex place-content-center w-1/3'>
-          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars">
+          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars" value={selected_base} onChange={ (e) => setSelectedBase(e.target.value) }>
               {
                 base_currency.map(ele => <option value={ele.code} key={ele.code}>{ele.description}</option>)
               }
