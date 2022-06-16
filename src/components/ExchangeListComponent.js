2a0b997f194cd770862d619ee44d71b8f4a21931
pyaephyomaung <ppmaungdev@gmail.com>
2022-06-16 20:59:31 +0630

    add table in exchange list and adjust for responsive

--- src/components/ExchangeListComponent.js
+++ src/components/ExchangeListComponent.js
@@ -42,21 +42,29 @@ const ExchangeListComponent = () => {
   return (
     <div className='w-full'>
       <div className='flex p-3 w-full place-content-center'>
-        <div className='flex place-content-center w-1/3'>
-          <select className='appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars" value={selected_base} onChange={ (e) => setSelectedBase(e.target.value) }>
+        <div className='flex place-content-center w-full md:w-2/5'>
+          <select className='w-full appearance-none px-2 py-2.5 leading-tight text-base font-medium text-left uppercase rounded border text-green-800 bg-white focus:outline-none focus:ring-0 focus:border-green-300 focus:shadow-md' name="cars" id="cars" value={selected_base} onChange={ (e) => setSelectedBase(e.target.value) }>
               {
                 base_currency.map(ele => <option value={ele.code} key={ele.code}>{ele.description}</option>)
               }
-              {/* <option value="saab">Thai Bhat</option> */}
           </select>
         </div>
+        {/* http://192.168.100.6:3000 */}
       </div>
-      <div className='flex place-content-center p-2 bg-red-500'>
-        <table className='bg-white w-2/3'>
-          <tr>
-            <td className='p-3'>United States Dollar</td>
-            <td className='p-3'>1</td>
-          </tr>
+      <div className='flex place-content-center py-2 px-3'>
+        <table className='w-full md:w-2/5 border-separate' style={{borderSpacing: 0}}>
+            <tr className='hover:bg-slate-100'>
+              <td className='px-5 py-3 w-3/5 border-y border-x rounded-tl text-left'>United States Dollar</td>
+              <td className='px-5 py-3 w-1/5 border-y border-r rounded-tr text-right'>1</td>
+            </tr>
+            <tr className='hover:bg-slate-100'>
+              <td className='px-5 py-3 w-3/5 border-b border-x text-left'>United States Dollar</td>
+              <td className='px-5 py-3 w-1/5 border-b border-r text-right'>1</td>
+            </tr>
+            <tr className='hover:bg-slate-100'>
+              <td className='px-5 py-3 w-3/5 border-x border-b rounded-bl text-left'>United States Dollar</td>
+              <td className='px-5 py-3 w-1/5 border-r border-b rounded-br text-right'>1</td>
+            </tr>
         </table>
       </div>
     </div>
